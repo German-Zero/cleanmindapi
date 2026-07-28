7d5db9645e73eac0df91c8d3cfe30ed6cb793cbd
German-Zero <germannaz_@hotmail.com>
2026-07-28 20:07:38 -0300

    se añadieron tres temas claros y movimiento de fondo para el user

--- apps/cleanmindapi/src/app/settings/application/commands/update-theme.command.ts
+++ apps/cleanmindapi/src/app/settings/application/commands/update-theme.command.ts
@@ -1,8 +1,10 @@
 import { Theme } from "../../domain/enums/theme.enum";
+import { BackgroundMotion } from "../../domain/enums/background-motion.enum";
 
 export class UpdateThemeCommand {
   constructor(
     public readonly userId: string,
     public readonly theme: Theme,
+    public readonly backgroundMotion?: BackgroundMotion,
   ) {}
 }
--- apps/cleanmindapi/src/app/settings/application/common/responses/user-settings.response.ts
+++ apps/cleanmindapi/src/app/settings/application/common/responses/user-settings.response.ts
@@ -1,9 +1,11 @@
 import { MotivationFrequency } from "../../../domain/enums/motivation-frequency.enum";
 import { NotificationFrequency } from "../../../domain/enums/notification-frequency.enum";
 import { Theme } from "../../../domain/enums/theme.enum";
+import { BackgroundMotion } from "../../../domain/enums/background-motion.enum";
 
 export class UserSettingsResponse {
   theme!: Theme;
+  backgroundMotion!: BackgroundMotion;
   emailNotifications!: boolean;
   whatsappNotifications!: boolean;
   discordNotifications!: boolean;
